795812aaa7f4ba32b52f281522975f2a1e113e38
Kervin Francois <mrkary2010@gmail.com>
2024-11-22 05:18:31 -0500

    Working in Lab 12

--- src/main/java/dao/DbConnectivityClass.java
+++ src/main/java/dao/DbConnectivityClass.java
@@ -17,7 +17,7 @@ public class DbConnectivityClass {
 
         private final ObservableList<Person> data = FXCollections.observableArrayList();
 
-        // Method to retrieve all data from the database and store it into an observable list to use in the GUI tableview.
+        // Method to retrieve all data from the database and store it into an observable list to use in the GUI tableview.w
 
 
         public ObservableList<Person> getData() {
