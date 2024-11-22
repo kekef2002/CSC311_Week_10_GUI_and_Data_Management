b0e7caf8c1cd65fe233df97abd6e9475069b3424
Kervin Francois <mrkary2010@gmail.com>
2024-11-22 05:12:43 -0500

    Working in Lab 12

---
+++ src/main/java/dao/StorageUploader.java
@@ -0,0 +1,26 @@
+package dao;
+
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobClientBuilder;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobContainerClientBuilder;
+
+public class StorageUploader {
+
+    private BlobContainerClient containerClient;
+
+    public StorageUploader( ) {
+        this.containerClient = new BlobContainerClientBuilder()
+                .connectionString("DefaultEndpointsProtocol=https;AccountName=francoisstorageaccount;AccountKey=DefaultEndpointsProtocol=https;AccountName=francoisstorageaccount;AccountKey=WDqzIKse4/7WTuOuj4bCToYnBnBDZiDbAsuxU/xALqbrDoDzXouCeh7MZRKKwLPsJhwm6RkXLoxS+AStlAi2Ug==;EndpointSuffix=core.windows.net")
+                .containerName("media-files")
+                .buildClient();
+    }
+
+    public void uploadFile(String filePath, String blobName) {
+        BlobClient blobClient = containerClient.getBlobClient(blobName);
+        blobClient.uploadFromFile(filePath);
+    }
+    public BlobContainerClient getContainerClient(){
+        return containerClient;
+    }
+}
--- src/main/java/viewmodel/DB_GUI_Controller.java
+++ src/main/java/viewmodel/DB_GUI_Controller.java
@@ -1,9 +1,12 @@
 package viewmodel;
 
+import com.azure.storage.blob.BlobClient;
 import dao.DbConnectivityClass;
+import dao.StorageUploader;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -20,15 +23,22 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import model.Person;
 import service.MyLogger;
+import javafx.scene.control.ProgressBar;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
 import java.net.URL;
+import java.nio.file.Files;
 import java.time.LocalDate;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class DB_GUI_Controller implements Initializable {
 
+    StorageUploader store =new StorageUploader();
+    @FXML
+    private ProgressBar progressBar;
     @FXML
     TextField first_name, last_name, department, major, email, imageURL;
     @FXML
@@ -228,5 +238,43 @@ public class DB_GUI_Controller implements Initializable {
             this.major = venue;
         }
     }
+    @FXML
+    protected void handleUploadButtonClick() {
+        File file = (new FileChooser()).showOpenDialog(progressBar.getScene().getWindow());
+        if (file != null) {
+            Task<Void> uploadTask = createUploadTask(file, progressBar);
+            progressBar.progressProperty().bind(uploadTask.progressProperty());
+            new Thread(uploadTask).start();
+        }
+    }
+
+    private Task<Void> createUploadTask(File file, ProgressBar progressBar) {
+        return new Task<>() {
+            @Override
+            protected Void call() throws Exception {
+                BlobClient blobClient = store.getContainerClient().getBlobClient(file.getName());
+                long fileSize = Files.size(file.toPath());
+                long uploadedBytes = 0;
+
+                try (FileInputStream fileInputStream = new FileInputStream(file);
+                     OutputStream blobOutputStream = blobClient.getBlockBlobClient().getBlobOutputStream()) {
+
+                    byte[] buffer = new byte[1024 * 1024]; // 1 MB buffer size
+                    int bytesRead;
+
+                    while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+                        blobOutputStream.write(buffer, 0, bytesRead);
+                        uploadedBytes += bytesRead;
+
+                        // Calculate and update progress as a percentage
+                        updateProgress(uploadedBytes, fileSize);
+                    }
+                }
+
+                return null;
+            }
+        };
+    }
+
 
 }
